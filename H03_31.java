7288effccc23d31e1d4af8fdf387db6507ab528e
Lia <moua0061@yahoo.com>
2022-06-14 02:00:16 -0400

    first commit

---
+++ H03_31.java
@@ -0,0 +1,11 @@
+public class H03_31 {
+
+    public int sum1toN(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+}
