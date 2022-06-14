9f5dc9affdef835042c43f4ea266939efade7cbd
Lia <moua0061@yahoo.com>
2022-06-14 02:11:04 -0400

    completed 3.1

--- H03_31.java
+++ H03_31.java
@@ -1,11 +1,16 @@
 public class H03_31 {
 
+    // public int sum1toN(int n) {
+    // int sum = 0;
+    // for (int i = 1; i <= n; i++) {
+    // sum += i;
+    // }
+    // return sum;
+    // }
     public int sum1toN(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+        if (n == 1)
+            return 1;
+        return sum1toN(n--);
     }
 
 }
